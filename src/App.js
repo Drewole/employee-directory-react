568b7beb4c3c66131d7db53d959672c03ed933e1
Drew Olsen <olsenj.andrew@gmail.com>
2021-05-04 23:08:12 -0500

    Components added. Scaffolding in app.js started

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Employee Directory</h1>
+        <p>Use the search box, or click on a column heading to sort by that column.</p>
       </header>
+      <main>
+        <div className="search">
+          <input type="text" name="search" id="search"/>
+          <button>Search</button>
+        </div>
+      </main>
+      
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
     </div>
   );
 }
